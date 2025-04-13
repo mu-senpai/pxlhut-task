1ffa9ba00f0d67da455188d19ce98299ebe04434
Abdulla Al Muhit <muhitabdullah279@gmail.com>
2025-04-13 10:30:32 +0530

    Height of the container has adjusted.

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -2,10 +2,26 @@ import Container from '@/components/Container'
 import FormWrapper from './FormWrapper'
 import "../globals.css"
 import ThemeToggle from '@/components/ThemeToggle'
+import { useEffect } from 'react';
 
 export default function FormPage() {
+
+    useEffect(() => {
+        const setFullHeight = () => {
+            document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+        };
+
+        setFullHeight();
+
+        window.addEventListener('resize', setFullHeight);
+
+        return () => {
+            window.removeEventListener('resize', setFullHeight);
+        };
+    }, []);
+
     return (
-        <section className='w-full h-screen relative flex flex-col items-center justify-center'>
+        <section className='w-full h-[calc(var(--vh,1vh)*100)] relative flex flex-col items-center justify-center'>
             <ThemeToggle />
             <Container>
                 <FormWrapper />
